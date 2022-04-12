93fa4ffe4b99b593a9841a10b3deda8ea8231f54
zaher-aa <zaherabuamro@gmail.com>
2022-04-12 21:08:41 +0300

    Added Navbar Relats #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Nav, Background } from './Components';
+import { Home, Movie, NotFound } from './Pages';
+import MovieContext from './Contexts';
 
 function App() {
-  return <>Hello, World!</>;
+  const [movie, setMovie] = useState({});
+  const movieMemo = useMemo(() => ({ movie, setMovie }), []);
+
+  return (
+    <MovieContext.Provider value={movieMemo}>
+      <Router>
+        <Nav />
+        <Background />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie" element={movie.name ? <Movie /> : <NotFound />} />
+        </Routes>
+      </Router>
+    </MovieContext.Provider>
+  );
 }
 
 export default App;
