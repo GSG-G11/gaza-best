898057cbe32fd1a0e4de5c8f92a34433c64252a3
AbdallahGot <abdallah897107@gmail.com>
2022-04-13 16:31:06 +0300

    add movie component reltes #6

--- src/Pages/Movie.jsx
+++ src/Pages/Movie.jsx
@@ -3,11 +3,40 @@ import PropTypes from 'prop-types';
 
 function Movie(props) {
   const { movie } = props;
-
-  console.log(movie);
+  const {
+    medium_cover_image: image,
+    title,
+    year,
+    genres,
+    torrents,
+    rating,
+    summary,
+    yt_trailer_code: trailerCode,
+  } = movie;
 
   return (
-    <div>hello, world</div>
+    <div className="container">
+      <div className="image-download">
+        <img src={image} alt={title} />
+        <a href={torrents[0].url}>Download</a>
+        <a href={`https://www.youtube.com/watch?v=${trailerCode}`}>Watch Trailer</a>
+      </div>
+      <div className="movie-details">
+        <h2>{title}</h2>
+        <p>{year}</p>
+        <p>{genres.join(' / ')}</p>
+        <div>
+          <p>Available in :</p>
+          { torrents.map((torrent) => (
+            <a href={torrent.url}>
+              {torrent.quality}
+            </a>
+          ))}
+          <p>{rating}</p>
+          <p>{summary}</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
